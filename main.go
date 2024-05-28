f39ef1c541f5b1194321f57afa15b9c10fd047f3
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-05-27 23:07:06 -0300

    working with multiple channels

--- main.go
+++ main.go
@@ -5,21 +5,27 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
 func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan)
 }
 
 func main() {
-	// go greet("Nice to meet you!")
-	// go greet("How are you?")
 	done := make(chan bool)
+	go greet("Nice to meet you!", done)
+	go greet("How are you?", done)
 	go slowGreet("How ... are ... you ...?", done)
-	// go greet("I hope you're liking the course!")
-	<-done
+	go greet("I hope you're liking the course!", done)
+
+	for range done {
+		//fmt.Println(doneChan)
+	}
+
 }
